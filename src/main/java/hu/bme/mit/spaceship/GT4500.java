8b22a28f286ca3bfef9055edf57a9799bea24bb8
mordekk <mordekk@gmail.com>
2018-05-04 02:34:48 -0700

    Changed requested things

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -67,17 +67,17 @@ public class GT4500 implements SpaceShip {
         }
         else {
           // try to fire the primary first
-          if (! primaryTorpedoStore.isEmpty()) {
-            wasPrimaryFiredLast = true;
-            return primaryTorpedoStore.fire(1);
-          }
-          else {
-            // although secondary was fired last time, but primary is empty
-            // thus try to fire secondary again
-            if (! secondaryTorpedoStore.isEmpty()) {
-              wasPrimaryFiredLast = false;
-              return secondaryTorpedoStore.fire(1);
-            }
+        	if (! primaryTorpedoStore.isEmpty()) {
+        		// although secondary was fired last time, but primary is empty
+        		// thus try to fire secondary again
+	        	if (! secondaryTorpedoStore.isEmpty()) {
+	        			wasPrimaryFiredLast = false;
+	        			return secondaryTorpedoStore.fire(1);
+	        	}
+	        	else {
+		            wasPrimaryFiredLast = true;
+		            return primaryTorpedoStore.fire(1);
+		        }
 
             // if both of the stores are empty, nothing can be done, return failure
           }
